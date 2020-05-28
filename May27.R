4c70ba35db731efdd88a5f8fd71c710d0daa09a3
Dan Feeney <daniel.feeney@colorado.edu>
2020-05-28 15:05:40 -0600

    adding more plots and some logic

--- May27.R
+++ May27.R
@@ -1,8 +1,13 @@
 library(tidyverse)
+library(ggridges)
 
 rm(list=ls())
 BC <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-05-26/boston_cocktails.csv')
-head(BC, 10)
+
+BC
+
+# Basic look --------------------------------------------------------------
+BC
 
 BC %>%
   group_by(ingredient) %>%
@@ -15,6 +20,30 @@ BC %>%
   summarize(
     nIngred = n()
   ) %>%
-  ggplot(mapping = aes(nIngred, fill = category)) + geom_histogram(stat="count") + facet_wrap(~ category)
+  ggplot(mapping = aes(nIngred, fill = category )) + geom_histogram(stat="count") + 
+  facet_wrap(~ category)
+
+BC %>%
+  group_by(name, category) %>%
+  summarize(
+    nIngred = n()
+  ) %>%
+  ggplot(mapping = aes(x = nIngred, y = category, fill = category )) + geom_density_ridges()
 
 
+# Figure out volume of cocktails ------------------------------------------
+
+
+BC %>% count(measure, sort = TRUE)
+# replace all of the fractions with decimals (taken from Hadley Wickham's YouTube)
+sizes <- BC %>%
+  filter(str_detect(measure, "oz")) %>%
+  mutate(oz = str_replace(measure, " oz", "")) %>%
+  mutate(oz = str_replace(oz, " ?1/2", "0.5")) %>%
+  mutate(oz = str_replace(oz, " ?1/4", "0.25")) %>%
+  mutate(oz = str_replace(oz, " ?3/4", "0.75")) %>%
+  mutate(oz = str_replace(oz, " ?2/3", "0.33")) %>%
+  mutate(oz = str_replace(oz, " ?2/3", "0.66")) 
+
+filter(sizes, oz > 5)
+ 
